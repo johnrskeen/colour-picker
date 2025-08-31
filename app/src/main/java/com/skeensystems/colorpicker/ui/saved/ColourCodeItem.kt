e8f2d051d94fab40027bf7047a34accef897f638
John Skeen <johnrskeen123@gmail.com>
2025-08-31 17:45:26 +0100

    Add composable to show details for a specific colour code

---
+++ app/src/main/java/com/skeensystems/colorpicker/ui/saved/ColourCodeItem.kt
@@ -0,0 +1,55 @@
+package com.skeensystems.colorpicker.ui.saved
+
+import android.content.ClipData
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.toClipEntry
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@Composable
+fun ColourCodeItem(
+    type: String,
+    value: String,
+    textColour: Color,
+) {
+    val clipboardManager = LocalClipboard.current
+    val scope = rememberCoroutineScope()
+
+    Row(
+        Modifier.fillMaxWidth().padding(10.dp).clickable {
+            scope.launch {
+                val clip = ClipData.newPlainText(type, "$type $value").toClipEntry()
+                clipboardManager.setClipEntry(clip)
+            }
+        },
+    ) {
+        Text(
+            modifier = Modifier.weight(1f).padding(horizontal = 10.dp),
+            text = type,
+            textAlign = TextAlign.Start,
+            color = textColour,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+        )
+        Text(
+            modifier = Modifier.weight(1f).padding(horizontal = 10.dp),
+            text = value,
+            textAlign = TextAlign.End,
+            color = textColour,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
