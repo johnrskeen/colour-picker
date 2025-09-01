219bedf47c82fe134afac28ae76bf49ff78f81d2
John Skeen <johnrskeen123@gmail.com>
2025-09-01 15:41:47 +0100

    Use utils copy colour function in action bar

--- app/src/main/java/com/skeensystems/colorpicker/ui/saved/CopyEditColourActionBar.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ui/saved/CopyEditColourActionBar.kt
@@ -1,6 +1,5 @@
 package com.skeensystems.colorpicker.ui.saved
 
-import android.content.ClipData
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,9 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboard
-import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.unit.dp
 import com.skeensystems.colorpicker.R
+import com.skeensystems.colorpicker.copyToClipboard
 import com.skeensystems.colorpicker.database.SavedColour
 import com.skeensystems.colorpicker.themeColour
 import kotlinx.coroutines.launch
@@ -33,15 +32,7 @@ fun CopyEditColourActionBar(inspectedColour: SavedColour) {
             contentColor = themeColour(R.attr.defaultTextColour),
             onClick = {
                 scope.launch {
-                    val text =
-                        inspectedColour.getName() +
-                            "\nHEX ${inspectedColour.getHEXString()}" +
-                            "\nHEX ${inspectedColour.getRGBString()}" +
-                            "\nHSV ${inspectedColour.getHSVString()}" +
-                            "\nHSL ${inspectedColour.getHSLString()}" +
-                            "\nCMYK ${inspectedColour.getCMYKString()}"
-                    val clip = ClipData.newPlainText("Colour", text).toClipEntry()
-                    clipboardManager.setClipEntry(clip)
+                    inspectedColour.copyToClipboard(clipboardManager = clipboardManager)
                 }
             },
             icon = { Icon(Icons.Filled.ContentCopy, "Copy colour details.") },
