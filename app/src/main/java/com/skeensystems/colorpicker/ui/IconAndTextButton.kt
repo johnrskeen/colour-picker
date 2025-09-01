c092831f66f6e6e505b68068a02a7c15545e6807
John Skeen <johnrskeen123@gmail.com>
2025-09-01 15:52:33 +0100

    Add button that can have an icon and text

---
+++ app/src/main/java/com/skeensystems/colorpicker/ui/IconAndTextButton.kt
@@ -0,0 +1,38 @@
+package com.skeensystems.colorpicker.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconAndTextButton(
+    modifier: Modifier,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    text: String,
+    contentDescription: String,
+    colour: Color,
+) {
+    Box(
+        modifier =
+            modifier.clickable {
+                onClick()
+            },
+    ) {
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(imageVector = icon, contentDescription = contentDescription, tint = colour)
+            Text(modifier = Modifier.padding(top = 10.dp), text = text, color = colour, fontWeight = FontWeight.Medium)
+        }
+    }
+}
