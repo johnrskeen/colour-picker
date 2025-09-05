da09dfeb124483418a57bbf6b8ab94b363a2acb7
John Skeen <johnrskeen123@gmail.com>
2025-09-05 20:00:08 +0100

    Add equals and hash code implementations for SavedColour
    This just relies on the id.

--- app/src/main/java/com/skeensystems/colorpicker/database/SavedColour.kt
+++ app/src/main/java/com/skeensystems/colorpicker/database/SavedColour.kt
@@ -188,5 +188,16 @@ class SavedColour : Colour {
         )
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SavedColour
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+
     override fun toString(): String = "Id: $id, Colour: $r, $g, $b, Favourite: $favorite"
 }
