e16ef77748d221cb1e1b8342511bbc6a5f8ee4f9
John Skeen <johnrskeen123@gmail.com>
2025-09-11 13:32:24 +0100

    Add picker screen

---
+++ app/src/main/java/com/skeensystems/colorpicker/ui/picker/PickerScreen.kt
@@ -0,0 +1,76 @@
+package com.skeensystems.colorpicker.ui.picker
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.skeensystems.colorpicker.R
+import com.skeensystems.colorpicker.themeColour
+import com.skeensystems.colorpicker.ui.picker.finedetails.FineDetailsContainer
+import com.skeensystems.colorpicker.ui.picker.slider.PickerSlider
+
+fun ComposeView.setPickerContent() {
+    setContent {
+        PickerScreen()
+    }
+}
+
+@Composable
+fun PickerScreen(viewModel: PickerViewModel = viewModel(LocalActivity.current as ComponentActivity)) {
+    val currentColour by viewModel.pickerColour.collectAsState()
+    val focusManager = LocalFocusManager.current
+    Scaffold(
+        modifier =
+            Modifier.pointerInput(Unit) {
+                detectTapGestures { focusManager.clearFocus() }
+            },
+    ) { paddingValues ->
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(themeColour(R.attr.mainColour))
+                    .padding(top = paddingValues.calculateTopPadding()),
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                GradientContainer(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .weight(4.25f)
+                            .padding(20.dp),
+                )
+                PickerSlider(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .height(40.dp)
+                            .padding(bottom = 10.dp, start = 20.dp, end = 20.dp),
+                )
+                FineDetailsContainer(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .weight(2.5f),
+                )
+                Box(modifier = Modifier.fillMaxWidth().weight(3f).background(currentColour))
+            }
+        }
+    }
+}
