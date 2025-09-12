7fd8dde8d927568983b10c8a051efdd704d704f3
John Skeen <johnrskeen123@gmail.com>
2025-09-12 11:38:28 +0100

    Add entity for saved colour
    This allows the saved colour object to have observable
    mutable states.

---
+++ app/src/main/java/com/skeensystems/colorpicker/database/SavedColourEntity.kt
@@ -0,0 +1,16 @@
+package com.skeensystems.colorpicker.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SavedColour")
+data class SavedColourEntity(
+    @PrimaryKey val id: Long,
+    @ColumnInfo(name = "r") val r: Int,
+    @ColumnInfo(name = "g") val g: Int,
+    @ColumnInfo(name = "b") val b: Int,
+    @ColumnInfo(name = "favorite") val favourite: Boolean,
+) {
+    fun toSavedColour(): SavedColour = SavedColour(id, r, g, b, favourite)
+}
