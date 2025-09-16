8cbae36ac1837d64a37ad9251e06dff8df862268
John Skeen <johnrskeen123@gmail.com>
2025-09-16 12:15:21 +0100

    Pass in dao to view model to load saved colours

--- app/src/main/java/com/skeensystems/colorpicker/MainViewModel.kt
+++ app/src/main/java/com/skeensystems/colorpicker/MainViewModel.kt
@@ -1,12 +1,29 @@
 package com.skeensystems.colorpicker
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.skeensystems.colorpicker.database.ColourDAO
 import com.skeensystems.colorpicker.database.SavedColour
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val colourDAO: ColourDAO,
+) : ViewModel() {
     private val _savedColours = mutableStateListOf<SavedColour>()
     val savedColours: List<SavedColour> = _savedColours
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            colourDAO.all
+                .forEach {
+                    _savedColours.add(it.toSavedColour())
+                }
+        }
+    }
 
     // TODO temporary, while colours are manually added, will be removed with full migration
     fun clearSavedColours() {
@@ -15,9 +32,27 @@ class MainViewModel : ViewModel() {
 
     fun addColour(savedColour: SavedColour) {
         _savedColours.add(savedColour)
+        viewModelScope.launch(Dispatchers.IO) {
+            colourDAO.insertAll(savedColour.toSavedColourEntity())
+        }
     }
 
     fun removeColour(savedColour: SavedColour) {
         _savedColours.remove(savedColour)
+        viewModelScope.launch(Dispatchers.IO) {
+            colourDAO.delete(savedColour.toSavedColourEntity())
+        }
+    }
+}
+
+class MainViewModelFactory(
+    private val colourDAO: ColourDAO,
+) : ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(colourDAO) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
