af1499aa5baba67796b1c9f54300936d8188698b
John Skeen <johnrskeen123@gmail.com>
2025-09-19 20:14:08 +0100

    Make picker r, g, b saved values float not int

--- app/src/main/java/com/skeensystems/colorpicker/userpreferences/PreferenceKeys.kt
+++ app/src/main/java/com/skeensystems/colorpicker/userpreferences/PreferenceKeys.kt
@@ -1,12 +1,12 @@
 package com.skeensystems.colorpicker.userpreferences
 
-import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferenceKeys {
     val SORT_STATUS = stringPreferencesKey("sort_status")
     val FILTER_STATUS = stringPreferencesKey("filter_status")
-    val PICKER_R = intPreferencesKey("picker_r")
-    val PICKER_G = intPreferencesKey("picker_g")
-    val PICKER_B = intPreferencesKey("picker_b")
+    val PICKER_R = floatPreferencesKey("picker_r")
+    val PICKER_G = floatPreferencesKey("picker_g")
+    val PICKER_B = floatPreferencesKey("picker_b")
 }
