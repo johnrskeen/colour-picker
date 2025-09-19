3a24c2b2eb872ec9f9d89b0e1e11c17d64686e41
John Skeen <johnrskeen123@gmail.com>
2025-09-19 12:10:17 +0100

    Use default text style to get proper measurements
    Add padding after modifier clickable element so
    hover highlight takes up whole button.

--- app/src/main/java/com/skeensystems/colorpicker/ui/saved/selectionmode/SelectionModeActionBar.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ui/saved/selectionmode/SelectionModeActionBar.kt
@@ -1,18 +1,19 @@
 package com.skeensystems.colorpicker.ui.saved.selectionmode
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.skeensystems.colorpicker.R
@@ -31,21 +32,23 @@ fun SelectionModeActionBar(
 
     var maxWidth by remember { mutableIntStateOf(0) }
 
-    val style = remember { TextStyle(fontWeight = FontWeight.Medium) }
+    val defaultStyle = LocalTextStyle.current
+    val style = remember { defaultStyle.copy(fontWeight = FontWeight.Medium) }
     maxWidth = actionBarItems.maxOf { measureTextWidth(it.text, style).value.toInt() }
 
     LazyRow(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 0.dp),
+        modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
         horizontalArrangement = Arrangement.SpaceEvenly,
     ) {
         items(actionBarItems) {
             IconAndTextButton(
-                modifier = Modifier.padding(20.dp).width(maxWidth.dp),
+                modifier = Modifier.width(maxWidth.dp + 20.dp),
                 onClick = it.onClick,
                 icon = it.icon,
                 text = it.text,
                 contentDescription = it.contentDescription,
                 colour = themeColour(R.attr.defaultTextColour),
+                afterOnClickPadding = PaddingValues(10.dp),
             )
         }
     }
