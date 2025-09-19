6821fcb736daf3d203da42078f1c35e0694cd477
John Skeen <johnrskeen123@gmail.com>
2025-09-20 00:34:07 +0100

    Add action bar for editing mode in manual picker

---
+++ app/src/main/java/com/skeensystems/colorpicker/ui/picker/EditingModeActionBar.kt
@@ -0,0 +1,69 @@
+package com.skeensystems.colorpicker.ui.picker
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.outlined.ChangeCircle
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.skeensystems.colorpicker.MainViewModel
+import com.skeensystems.colorpicker.R
+import com.skeensystems.colorpicker.database.SavedColour
+import com.skeensystems.colorpicker.themeColour
+
+@Composable
+fun EditingModeActionBar(
+    modifier: Modifier = Modifier,
+    viewModel: MainViewModel = viewModel(LocalActivity.current as ComponentActivity),
+    editingColour: SavedColour,
+    currentColour: Color,
+    exitEditingMode: (SavedColour) -> Unit,
+) {
+    Row(modifier = modifier.fillMaxWidth().padding(10.dp)) {
+        ExtendedFloatingActionButton(
+            modifier = Modifier.weight(1f).padding(5.dp),
+            containerColor = themeColour(R.attr.mainColour),
+            contentColor = themeColour(R.attr.defaultTextColour),
+            onClick = {
+                val newColour =
+                    viewModel.saveColour(
+                        (currentColour.red * 255).toInt(),
+                        (currentColour.green * 255).toInt(),
+                        (currentColour.blue * 255).toInt(),
+                    )
+                exitEditingMode(newColour)
+            },
+            icon = { Icon(Icons.Filled.ContentCopy, "Save copy.") },
+            text = { Text("Save copy") },
+        )
+        ExtendedFloatingActionButton(
+            modifier = Modifier.weight(1f).padding(5.dp),
+            containerColor = themeColour(R.attr.mainColour),
+            contentColor = themeColour(R.attr.defaultTextColour),
+            onClick = {
+                viewModel.deleteColour(editingColour)
+                val newColour =
+                    viewModel.saveColour(
+                        r = (currentColour.red * 255).toInt(),
+                        g = (currentColour.green * 255).toInt(),
+                        b = (currentColour.blue * 255).toInt(),
+                        id = editingColour.id,
+                        favourite = editingColour.favourite,
+                    )
+                exitEditingMode(newColour)
+            },
+            icon = { Icon(Icons.Outlined.ChangeCircle, "Confirm.") },
+            text = { Text("Confirm") },
+        )
+    }
+}
