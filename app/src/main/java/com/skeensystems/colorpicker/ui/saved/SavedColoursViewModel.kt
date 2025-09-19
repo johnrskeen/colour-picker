32735c210bacf37c79f3b9ddaa0309973fc5c638
John Skeen <johnrskeen123@gmail.com>
2025-09-19 19:27:03 +0100

    Add sort and filter status to saved colours view model

--- app/src/main/java/com/skeensystems/colorpicker/ui/saved/SavedColoursViewModel.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ui/saved/SavedColoursViewModel.kt
@@ -1,26 +1,41 @@
 package com.skeensystems.colorpicker.ui.saved
 
+import android.app.Application
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
+import androidx.datastore.preferences.core.edit
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.skeensystems.colorpicker.database.SavedColour
 import com.skeensystems.colorpicker.ui.saved.expandeddetails.VisibilityStatus
+import com.skeensystems.colorpicker.ui.saved.sortandfilter.FilterOptions
+import com.skeensystems.colorpicker.ui.saved.sortandfilter.SortOptions
+import com.skeensystems.colorpicker.userpreferences.PreferenceKeys
+import com.skeensystems.colorpicker.userpreferences.dataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
-class SavedColoursViewModel : ViewModel() {
+class SavedColoursViewModel(
+    application: Application,
+) : AndroidViewModel(application) {
     private val _selectingMode = mutableStateOf(false)
     val selectingMode: State<Boolean> = _selectingMode
 
     private val _selectedItems = mutableStateSetOf<SavedColour>()
     val selectedItems: Set<SavedColour> = _selectedItems
 
-    private val _visibilityStatus =
-        mutableStateOf<VisibilityStatus>(
-            VisibilityStatus.Hide(Offset(0f, 0f)),
-        )
+    private val _visibilityStatus = mutableStateOf<VisibilityStatus>(VisibilityStatus.Hide(Offset(0f, 0f)))
     val visibilityStatus: State<VisibilityStatus> = _visibilityStatus
+
+    private val _sortStatus = mutableStateOf(SortOptions.NEWEST_FIRST)
+    val sortStatus: State<SortOptions> = _sortStatus
+
+    private val _filterStatus = mutableStateOf(FilterOptions.NO_FILTER)
+    val filterStatus: State<FilterOptions> = _filterStatus
 
     val savedColourCoordinates = mutableMapOf<Long, Offset>()
 
@@ -28,6 +43,33 @@ class SavedColoursViewModel : ViewModel() {
     var screenWidth = 0.dp
     var screenHeight = 0.dp
     var colourViewDimension = 0.dp
+
+    private val context = getApplication<Application>()
+
+    init {
+        viewModelScope.launch {
+            _sortStatus.value =
+                when (
+                    context.dataStore.data
+                        .map { prefs -> prefs[PreferenceKeys.SORT_STATUS] ?: "Newest first" }
+                        .first()
+                ) {
+                    "Newest first" -> SortOptions.NEWEST_FIRST
+                    "Oldest first" -> SortOptions.OLDEST_FIRST
+                    else -> SortOptions.BY_COLOUR
+                }
+
+            _filterStatus.value =
+                when (
+                    context.dataStore.data
+                        .map { prefs -> prefs[PreferenceKeys.FILTER_STATUS] ?: "No filter" }
+                        .first()
+                ) {
+                    "No filter" -> FilterOptions.NO_FILTER
+                    else -> FilterOptions.FAVOURITES
+                }
+        }
+    }
 
     fun enterSelectingMode(savedColour: SavedColour) {
         _selectingMode.value = true
@@ -50,5 +92,23 @@ class SavedColoursViewModel : ViewModel() {
 
     fun setVisibilityStatus(visibilityStatus: VisibilityStatus) {
         _visibilityStatus.value = visibilityStatus
+    }
+
+    fun setSortStatus(sortStatus: SortOptions) {
+        _sortStatus.value = sortStatus
+        viewModelScope.launch {
+            context.dataStore.edit { preferences ->
+                preferences[PreferenceKeys.SORT_STATUS] = sortStatus.label
+            }
+        }
+    }
+
+    fun setFilterStatus(filterStatus: FilterOptions) {
+        _filterStatus.value = filterStatus
+        viewModelScope.launch {
+            context.dataStore.edit { preferences ->
+                preferences[PreferenceKeys.FILTER_STATUS] = filterStatus.label
+            }
+        }
     }
 }
