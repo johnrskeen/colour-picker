fb06fb66afeb9f54da474dc93500eabda64fc4ac
John Skeen <johnrskeen123@gmail.com>
2025-09-20 15:54:57 +0100

    Just adding this so it is in git history
    This shows how to add extended themes.
    LocalColourScheme.current.COLOUR_NAME

--- app/src/main/java/com/skeensystems/colorpicker/ColourPickerTheme.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ColourPickerTheme.kt
@@ -1,6 +1,8 @@
 package com.skeensystems.colorpicker
 
+import android.content.Context
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -8,6 +10,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
@@ -21,17 +27,64 @@ fun ColourPickerTheme(
         when {
             dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
                 if (darkTheme) {
-                    dynamicDarkColorScheme(context)
+                    dynamicDarkColorScheme(
+                        context,
+                    ).copy(background = Color(context.resources.getColor(android.R.color.system_neutral2_900, context.theme)))
                 } else {
-                    dynamicLightColorScheme(context)
+                    dynamicLightColorScheme(
+                        context,
+                    ).copy(background = Color(context.resources.getColor(android.R.color.system_neutral2_100, context.theme)))
                 }
             }
-            darkTheme -> darkColorScheme()
-            else -> lightColorScheme()
+            darkTheme -> darkColorScheme().copy(primary = Color.LightGray, background = Color.DarkGray)
+            else -> lightColorScheme().copy(primary = Color.Black, background = Color.LightGray)
         }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        content = content,
-    )
+    val extendedColorScheme =
+        when {
+            dynamicColor || Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                if (darkTheme) {
+                    dynamicExtendedDark(context)
+                } else {
+                    dynamicExtendedLight(context)
+                }
+            }
+            darkTheme -> extendedDark()
+            else -> extendedLight()
+        }
+
+    CompositionLocalProvider(
+        LocalColourScheme provides extendedColorScheme,
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content,
+        )
+    }
 }
+
+@Immutable
+data class ExtendedColorScheme(
+    val background: Color,
+)
+
+@RequiresApi(Build.VERSION_CODES.S)
+fun dynamicExtendedLight(context: Context) =
+    ExtendedColorScheme(
+        background = Color(context.resources.getColor(android.R.color.system_neutral2_100, context.theme)),
+    )
+
+@RequiresApi(Build.VERSION_CODES.S)
+fun dynamicExtendedDark(context: Context) =
+    ExtendedColorScheme(background = Color(context.resources.getColor(android.R.color.system_neutral2_900, context.theme)))
+
+fun extendedLight() = ExtendedColorScheme(background = Color.LightGray)
+
+fun extendedDark() = ExtendedColorScheme(background = Color.DarkGray)
+
+val LocalColourScheme =
+    staticCompositionLocalOf {
+        ExtendedColorScheme(
+            background = Color.White,
+        )
+    }
