4a26e652cc7ef6e9b3caf3789fb8461bba5776ec
John Skeen <johnrskeen123@gmail.com>
2025-09-20 15:28:06 +0100

    Remove setting text colour
    This is now handled automatically by the theme.

--- app/src/main/java/com/skeensystems/colorpicker/ui/picker/finedetails/AdjustButtons.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ui/picker/finedetails/AdjustButtons.kt
@@ -10,22 +10,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.skeensystems.colorpicker.R
-import com.skeensystems.colorpicker.themeColour
 
 @Composable
 fun AdjustButtons(
     modifier: Modifier = Modifier,
     onAdjust: (Int) -> Unit,
 ) {
-    val textColour = themeColour(R.attr.defaultTextColour)
     Row(modifier = modifier) {
         Box(modifier = Modifier.weight(1f).clickable { onAdjust(-1) }) {
             Icon(
                 modifier = Modifier.align(Alignment.Center),
                 imageVector = Icons.Filled.KeyboardArrowDown,
                 contentDescription = "Minus 1.",
-                tint = textColour,
             )
         }
         Box(modifier = Modifier.weight(1f).clickable { onAdjust(1) }) {
@@ -33,7 +29,6 @@ fun AdjustButtons(
                 modifier = Modifier.align(Alignment.Center),
                 imageVector = Icons.Filled.KeyboardArrowUp,
                 contentDescription = "Add 1.",
-                tint = textColour,
             )
         }
     }
