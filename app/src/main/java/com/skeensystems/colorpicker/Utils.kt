243b8e95878ff986b49a825860ca5e556cae8176
John Skeen <johnrskeen123@gmail.com>
2025-09-21 13:34:32 +0100

    Formatting change, put all on one line

--- app/src/main/java/com/skeensystems/colorpicker/Utils.kt
+++ app/src/main/java/com/skeensystems/colorpicker/Utils.kt
@@ -25,10 +25,7 @@ fun Color.getClosestColour(viewModel: MainViewModel): String =
     viewModel.getClosestColour(
         (red * 255).toInt(),
         (green * 255).toInt(),
-        (
-            blue *
-                255
-        ).toInt(),
+        (blue * 255).toInt(),
     )
 
 fun Color.getHexString(): String = String.format("#%02X%02X%02X", (red * 255).toInt(), (green * 255).toInt(), (blue * 255).toInt())
