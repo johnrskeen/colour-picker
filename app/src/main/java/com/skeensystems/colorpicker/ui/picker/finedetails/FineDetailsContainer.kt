df746f1b15a7e817b0b17333a035c7e4fc8991a3
John Skeen <johnrskeen123@gmail.com>
2025-09-21 14:43:56 +0100

    Use tabs above instead of sidebar and buttons
    This looks better and is more space efficient.
    It will also allow for entering with more colour systems.

--- app/src/main/java/com/skeensystems/colorpicker/ui/picker/finedetails/FineDetailsContainer.kt
+++ app/src/main/java/com/skeensystems/colorpicker/ui/picker/finedetails/FineDetailsContainer.kt
@@ -1,58 +1,38 @@
 package com.skeensystems.colorpicker.ui.picker.finedetails
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
-// TODO add temporary colours to replace R.attr as buttons will be replaced with tabs
 @Composable
 fun FineDetailsContainer(modifier: Modifier = Modifier) {
-    val pagerState = rememberPagerState(pageCount = { 3 })
-    val scope = rememberCoroutineScope()
-    Row(modifier = modifier) {
-        Column(modifier = Modifier.fillMaxHeight().wrapContentWidth()) {
-            listOf(ColourSystem.RGB, ColourSystem.HEX, ColourSystem.HSV).forEachIndexed { index, title ->
-                Button(
-                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
-                    colors =
-                        ButtonDefaults.buttonColors(
-                            containerColor =
-                                if (pagerState.currentPage ==
-                                    index
-                                ) {
-                                    Color.LightGray // themeColour(R.attr.reversedColourAccent)
-                                } else {
-                                    Color.DarkGray // themeColour(R.attr.mainColourAccent)
-                                },
-                        ),
-                    modifier =
-                        Modifier
-                            .weight(1f)
-                            .padding(4.dp),
-                ) {
-                    Text(title.name, maxLines = 1)
-                }
+    var selectedTab by remember { mutableIntStateOf(0) }
+
+    Column(modifier = modifier) {
+        TabRow(selectedTabIndex = selectedTab) {
+            listOf(
+                ColourSystem.RGB,
+                ColourSystem.HEX,
+                ColourSystem.HSV,
+            ).forEachIndexed { index, title ->
+                Tab(
+                    selected = selectedTab == index,
+                    onClick = { selectedTab = index },
+                    text = { Text(title.name) },
+                )
             }
         }
-        VerticalPager(modifier = Modifier.fillMaxHeight().weight(1f), state = pagerState) { page ->
-            when (page) {
-                0 -> FineDetailsPage(colourSystem = ColourSystem.RGB)
-                1 -> FineDetailsPage(colourSystem = ColourSystem.HEX)
-                else -> FineDetailsPage(colourSystem = ColourSystem.HSV)
-            }
+        when (selectedTab) {
+            0 -> FineDetailsPage(modifier = Modifier.weight(1f), colourSystem = ColourSystem.RGB)
+            1 -> FineDetailsPage(modifier = Modifier.weight(1f), colourSystem = ColourSystem.HEX)
+            2 -> FineDetailsPage(modifier = Modifier.weight(1f), colourSystem = ColourSystem.HSV)
         }
     }
 }
